44e4a523432fbed6c94385bd0597fb470d729ac2
Nadeem Mohsin <nadeemm@verily.com>
2026-05-04 13:56:04 -0700

    Switch to ZSH.

--- modules/users.nix
+++ modules/users.nix
@@ -3,6 +3,7 @@
 {
   users.users.nadeem = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCVa9SU6Uk6T9oOMQXyoaZ6pr5dUzTkS5N/YIKVm3VH abstractwhiz@gmail.com"
