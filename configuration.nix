9a5a206db5973604fb9a3e590a5dbec3e8514555
Nadeem Mohsin <nadeemm@verily.com>
2026-05-04 14:18:01 -0700

    Switch to LTS packages.

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   hardware.enableRedistributableFirmware = true;
 
