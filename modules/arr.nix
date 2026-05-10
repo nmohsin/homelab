e838ce1bcef28086390a2d5b98a1e09a510b5f78
Nadeem Mohsin <nadeemm@verily.com>
2026-05-09 21:47:52 -0700

    Add FlareSolverr.

--- modules/arr.nix
+++ modules/arr.nix
@@ -5,11 +5,22 @@
   services.sonarr.enable = true;
   services.radarr.enable = true;
   services.jellyfin.enable = true;
+
+  virtualisation.oci-containers.containers.flaresolverr = {
+    image = "ghcr.io/flaresolverr/flaresolverr";
+    environment = {
+      LOG_LEVEL = "info";
+      TZ = "America/Los_Angeles";
+    };
+    ports = [ "8191:8191" ];
+  };
+
   networking.firewall.allowedTCPPorts = [
     8096  # Jellyfin
     8989  # Sonarr
     7878  # Radarr
     9696  # Prowlarr
     8080  # qBittorrent
+    8191  # FlareSolverr
   ];
 }
